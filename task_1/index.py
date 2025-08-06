2db94800e84626af256977d05a094de1c0182cdf
bogdan_kopytko <86629311+bogdankopytko@users.noreply.github.com>
2025-08-06 22:57:11 +0200

    improving cod by reduce the using if's.

--- task_1/index.py
+++ task_1/index.py
@@ -2,15 +2,15 @@ def caching_fibonacci():
     cache = {}
 
     def fibonacci(n):
-        if n not in cache:
-            if n <= 0:
-                return 0
-            elif n == 1:
-                return 1
-            else:
-                cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-                return cache[n]
+        if n in cache:
+            return cache[n]
+
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
         else:
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
             return cache[n]
 
     return fibonacci
